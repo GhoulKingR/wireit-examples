28bbadc692774bc6fba5baf38453f5a75f395af1
Chigozie Oduah <oduahchigozie46@gmail.com>
2023-07-15 13:13:17 +0100

    added files options

---
+++ express-app-js/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (_req, res) => {
+    res.send("Hello, World")
+});
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Application started: listening on port ${port}`);
+});
